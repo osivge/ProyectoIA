c6b9bee65937b79c34859a198eaa339a597c37c0
oscarivan_vg <oscarivan_vg@hotmail.com>
2016-02-25 17:07:29 -0600

    Cambios a CostMov.java
    La tabla ahora inicia con una criatura y 8 terrenos
    
    Se agrega la funcion obtenerCostosCriatura que regresa una lista con los
    costos por terreno de la criatura especificada.

--- ProyectoIA/src/com/modelo/CostMov.java
+++ ProyectoIA/src/com/modelo/CostMov.java
@@ -1,43 +1,127 @@
 package com.modelo;
 
+import java.awt.Color;
 import java.util.ArrayList;
 
-public class CostMov {
-	public static final CostMov singTab = new CostMov();
-	public ArrayList<ArrayList<Object>> array = new ArrayList<ArrayList<Object>>();
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
+
+public class CostMov extends JTable{
 	
 	public CostMov(){
-		array.add(new ArrayList<Object>());
-		array.get(0).add(".");
-		array.get(0).add(".");
-		array.add(new ArrayList<Object>());
-		array.get(1).add(".");
-		array.get(1).add(".");
+		modelo = (DefaultTableModel)this.getModel();
+		this.setModel(modelo);
+		modelo.addColumn(columnNames);
+		this.getColumnModel().getColumn(0).setWidth(30);
+		modelo.addRow(cells);
+		modelo.addColumn(columnNames);
+		this.getColumnModel().getColumn(1).setWidth(30);
+		modelo.addRow(cells);
+		modelo.addColumn(columnNames);
+		this.getColumnModel().getColumn(2).setWidth(30);
+		modelo.addRow(cells);
+		this.setValueAt("ID", 0, 0);
+		this.setValueAt("Nombre", 1, 1);
+		this.setValueAt("Terrenos", 2, 1);
+		this.setValueAt("Criaturas", 1, 2);
+		this.setValueAt("Color", 2, 2);
+		System.out.println(modelo.getDataVector());
+		
+		this.agregarCriatura();
+		this.setValueAt("Humano", 1, contCriaturas+2);
+		this.setValueAt(Color.decode("0x660033"), 2, contCriaturas+2);
+		
+		this.agregarTerreno();
+		this.setValueAt("Montaña", contTerrenos+2, 1);
+		this.setValueAt(Color.decode("0x660033"), contTerrenos+2, 2);
+		
+		this.agregarTerreno();
+		this.setValueAt("Camino", contTerrenos+2, 1);
+		this.setValueAt(Color.decode("0xFFCC66"), contTerrenos+2, 2);
+ 
+		this.agregarTerreno();
+		this.setValueAt("Agua", contTerrenos+2, 1);
+		this.setValueAt(Color.decode("0x3399FF"), contTerrenos+2, 2);
+
+		this.agregarTerreno();
+		this.setValueAt("Arena", contTerrenos+2, 1);
+		this.setValueAt(Color.decode("0xFFFF99"), contTerrenos+2, 2);
+		
+		this.agregarTerreno();
+		this.setValueAt("Bosque", contTerrenos+2, 1);
+		this.setValueAt(Color.decode("0x009933"), contTerrenos+2, 2);
+		
+		this.agregarTerreno();
+		this.setValueAt("Pantano", contTerrenos+2, 1);
+		this.setValueAt(Color.decode("0x666633"), contTerrenos+2, 2);
+	
+		this.agregarTerreno();
+		this.setValueAt("Nieve", contTerrenos+2, 1);
+		this.setValueAt(Color.decode("0xFFFFFF"), contTerrenos+2, 2);
+		
+		this.agregarTerreno();
+		this.setValueAt("Pared", contTerrenos+2, 1);
+		this.setValueAt(Color.BLACK, contTerrenos+2, 2);
 	}
 	
-	public void agregarCriatura(ArrayList<Object> listaCosto){
-		for(int i=0 ; i < array.size() ; i++){
-        	array.get(i).add(listaCosto.get(i));
-        }
+	public Object obtenerIdTerreno(int indTerreno){
+		return this.getValueAt(indTerreno+3, 0);
 	}
 	
-	public void agregarTerreno(ArrayList<Object> listaCosto){
-		array.add(new ArrayList<Object>());
-		for(int i = 0 ; i < array.get(0).size() ; i++){
-			array.get(array.size()-1).add(listaCosto.get(i));
+	public Object obtenerNombreTerreno(int indTerreno){
+		return this.getValueAt(indTerreno+3, 1);
+	}
+	
+	public Object obtenerColorTerreno(int indTerreno){
+		return this.getValueAt(indTerreno+3, 2);
+	}
+	
+	public Object obtenerIdCriatura(int indCriatura){
+		return this.getValueAt(0, indCriatura+3);
+	}
+	
+	public Object obtenerNombreCriatura(int indCriatura){
+		return this.getValueAt(1, indCriatura+3);
+	}
+	
+	public Object obtenerColorCriatura(int indCriatura){
+		return this.getValueAt(2, indCriatura+3);
+	}
+	
+	public Object obtenerCosto(int indTerreno, int indCriatura){
+		return this.getValueAt(indTerreno+3, indCriatura+3);
+	}
+	public ArrayList<Integer> obtenerCostosCriatura(int indCriatura){
+		ArrayList<Integer> costos = new ArrayList<Integer>();
+		for(int i = 3; i<this.getRowCount();i++){
+			costos.add(Integer.parseInt((String)this.getValueAt(i, indCriatura+3)));
+		}
+		return costos;
+	}
+	
+	public void agregarCriatura(){
+		modelo.addColumn("");
+		this.setValueAt(contCriaturas++, 0, modelo.getColumnCount()-1);
+		for(int i = 3; i<this.getRowCount();i++){
+			this.setValueAt("-1", i, modelo.getColumnCount()-1);
 		}
 	}
 	
-	public String toString(){
-		String str = "";
-		for(int i=0;i<array.size();i++){  //(para cada fila)
-			str = "";
-            for(int j=0;j<array.get(i).size();j++){  //se recorre todas la columnas de la fila
-                str += array.get(i).get(j); //se obtiene el elemento i,j
-            }
-            System.out.println(str);
+	public void agregarTerreno(){
+		modelo.addRow(new Object[modelo.getColumnCount()]);
+		this.setValueAt(contTerrenos++, modelo.getRowCount()-1, 0);
+		for(int i = 3; i<this.getColumnCount();i++){
+			this.setValueAt("-1", modelo.getRowCount()-1, i);
 		}
-		return str;
 	}
 	
+	private int contCriaturas = 0;
+	
+	private int contTerrenos = 0;
+	
+	public DefaultTableModel modelo;
+	
+	private Object[] cells = {null};
+	
+	private String columnNames = "";
 }
